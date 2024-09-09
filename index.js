5ff3b1959264629fa001f6bdf93618b4cda8b6b6
ObiFaith <obifth@gmail.com>
2024-09-09 07:18:13 +0100

    refactor: add comments in index.js

--- index.js
+++ index.js
@@ -4,66 +4,85 @@ const tips = Array.from(document.querySelectorAll('.tip'));
 const inputs = Array.from(document.querySelectorAll('input'));
 const tipAmt = document.querySelector('#tip-amt').firstElementChild;
 const totalAmt = document.querySelector('#total-amt').firstElementChild;
-let numberOfPersons = 1,
-	prev_value,
-	prev_percent;
 
-const calcTipAndTotalAmt = (value, percent) => {
-	tipAmt.innerText = (
-		(value * percent.replace('%', '')) /
-		(100 * numberOfPersons)
-	).toFixed(2);
+let numberOfPersons = 1,
+	prev_bill,
+	prev_tip;
+
+const calcTipAndTotalAmt = (bill, tip) => {
+	// remove % to get tip value
+	const tipValue = tip.replace('%', '');
+	// calculate tip amount (in 2dp)
+	tipAmt.innerText = ((bill * tipValue) / 100 / numberOfPersons).toFixed(2);
+
+	// calculate total amount
 	totalAmt.innerText =
 		Number(tipAmt.innerText) + Number(inputs[0].value) / numberOfPersons;
+	// convert total amount to 2dp (decimal point)
 	totalAmt.innerText = Number(totalAmt.innerText).toFixed(2);
+
+	// style each tip elements
 	tips.forEach(tip => {
-		if (tip.innerText == percent) {
+		// style for selected tip element
+		if (tip.innerText == tip) {
 			tip.style.backgroundColor = 'hsl(172, 67%, 45%)';
 			tip.style.color = 'hsl(183, 100%, 15%)';
-		} else {
+		}
+		// style for other tip elements
+		else {
 			tip.style.backgroundColor = 'hsl(183, 100%, 15%)';
 			tip.style.color = '#FFF';
 		}
 	});
-	prev_value = value;
-	prev_percent = percent;
+
+	prev_bill = bill;
+	prev_tip = tip;
 };
 
+// calculate tip amount and total amount when there's a change in bill (inputted) value
 inputs[0].addEventListener('input', () =>
-	calcTipAndTotalAmt(inputs[0].value, prev_percent)
+	calcTipAndTotalAmt(inputs[0].value, prev_tip)
 );
 
+// calculate tip and total amount for custom tip value
 inputs[1].addEventListener('input', () =>
 	calcTipAndTotalAmt(inputs[0].value, inputs[1].value)
 );
 
+// set tip and total amount to zeros
 const reset = () => {
 	tipAmt.innerText = '0.00';
 	totalAmt.innerText = '0.00';
 };
 
 inputs[2].addEventListener('input', () => {
+	// when number of people is < 1 or empty, reset the tip and total amount to zero
 	if (inputs[2].value < 1 || inputs[2].value.trim() === '') reset();
 	else {
-		numberOfPersons = inputs[2].value !== 1 ? Number(inputs[2].value) : 1;
-		calcTipAndTotalAmt(prev_value, prev_percent);
+		numberOfPersons = inputs[2].value;
+		// using the prev bill and tip, calc tip amt and total amt now that the value of numberOfPerson have changed
+		calcTipAndTotalAmt(prev_bill, prev_tip);
 	}
+
 	if (inputs[2].value == 0) {
+		// input style for invalid number of people
 		zero.style.display = 'block';
 		inputs[2].style.borderColor = '#FC3A3A';
 	} else {
+		// default input number of people
 		zero.style.display = 'none';
 		inputs[2].style.borderColor = 'hsl(172, 67%, 45%)';
 	}
 });
 
+// calculate the tip and total amount when a tip is clicked
 tips.forEach(tip => {
-	tip.addEventListener('click', e => {
+	tip.addEventListener('click', () => {
 		calcTipAndTotalAmt(inputs[0].value, tip.innerText);
-		inputs[1].value = '';
 	});
 });
 
+// set tip amout, total amount and input values to zeros
 resetBtn.addEventListener('click', () => {
 	reset();
 	inputs.forEach(input => (input.value = ''));
